a2449d245f9a3b85150694a8f9c7df892b168ee0
tuncay <tuzunuz@corp.vodafone.es>
2023-02-01 23:21:09 +0300

    initial commit

---
+++ src/main/java/com/tallstech/hizir/material/HizirAidMaterialServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tallstech.hizir.material;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HizirAidMaterialServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HizirAidMaterialServiceApplication.class, args);
+	}
+
+}
